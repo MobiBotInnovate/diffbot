ccd51653f048a82386043d4272a19f6c629e63fd
Pontus <pontus@pontuss.com>
2024-05-21 16:54:22 +0200

    motor driver launch file

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
             output="screen",
             parameters=[{
                 "serial_baudrate": 115200,
-                "serial_port": "/dev/ttyUSB1",
+                "serial_port": "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0",
                 "frame_id": "laser_frame",
                 "angle_compensate": True,
                 "scan_mode": "Standard"
