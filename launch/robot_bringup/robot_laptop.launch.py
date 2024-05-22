9a223a7a5672a7371dea623e4c45ba3e6d4545a1
Pontus Svensson <psn19003@student.mdu.se>
2024-05-22 18:35:46 +0200

    start slam and  nav2

--- launch/robot_bringup/robot_laptop.launch.py
+++ launch/robot_bringup/robot_laptop.launch.py
@@ -1,22 +1,25 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     package_name = "diffbot"
     package_share_directory = get_package_share_directory(package_name)
+    slam_toolbox_file = os.path.join(
+        package_share_directory, "launch/slam_bringup", "online_async_launch.py"
+    )
+    nav2_toolbox_file = os.path.join(
+        package_share_directory, "launch/nav_bringup", "navigation_launch.py"
+    )
 
     rviz_params = os.path.abspath(
         os.path.join(package_share_directory, "config", "nav2_default_view.rviz")
     )
-    twist_mux_params_file = os.path.abspath(
-        os.path.join(package_share_directory, "config", "twist_mux.yaml")
-    )
-
     rviz = Node(
         package="rviz2",
         executable="rviz2",
@@ -24,18 +27,20 @@ def generate_launch_description():
         arguments=["-d", rviz_params],
         output={"both": "log"},
     )
-
-    twix_mux = Node(
-        package="twist_mux",
-        executable="twist_mux",
-        parameters=[twist_mux_params_file],
-        remappings=[("/cmd_vel_out", "/diff_cont/cmd_vel_unstamped")],
+    # Include SLAM launch file
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(slam_toolbox_file)
     )
 
+    # Include Nav2 launch file
+    nav2_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(nav2_toolbox_file)
+    )
     return LaunchDescription(
         [
-            twix_mux,
             rviz,
+            slam_toolbox,
+            nav2_toolbox,
         ]
     )
 
